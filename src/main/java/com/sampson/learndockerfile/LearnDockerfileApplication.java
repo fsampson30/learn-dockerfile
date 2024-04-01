608447668d21a7ac1698d568913095dda655a3ad
fsampson30 <fsampson30@gmail.com>
2024-04-01 07:56:40 -0300

    Initial commit

---
+++ src/main/java/com/sampson/learndockerfile/LearnDockerfileApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.learndockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDockerfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDockerfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampson/learndockerfile/LearnDockerfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.learndockerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDockerfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
